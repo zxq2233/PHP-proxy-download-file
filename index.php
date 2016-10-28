656684e48423b2209284fe2a05ce77cb3d7ab548
zxq2233 <531373473@qq.com>
2016-10-28 15:41:23 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP PROXY DOWNLOAD FILE</title>
+</head>
+<body>
+<form action="down.php">
+<input type="text" name="url"><br>
+  <input type="submit" value="DOWN">
+</form>
+</body>
+</html>
